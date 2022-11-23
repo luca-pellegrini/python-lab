7c7cca0ba022868dbc86bb88b6cc02939ebcd0a0
Luca Pellegrini <luca.pellegrini@disroot.org>
2022-11-23 22:49:22 +0100

    Completata esercitazione 5, del 16/11/2022

--- E5-16.11.2022/e5.8_number_name.py
+++ E5-16.11.2022/e5.8_number_name.py
@@ -3,6 +3,7 @@
 from my_input import *
 
 ## Restituisce il nome dell'unità
+#  Usata solo all'interno di altre funzioni di seguito definite
 #  @param num Numero intero da analizzare
 #  @return Stringa
 #
@@ -27,18 +28,18 @@ def nomeDecineUnita(num) :
     c_unita = num % 10
     c_decine = (num // 10) % 10
     s = '' # stringa da restituire
-    if c_decine == 0 : return nomeUnita(num)
+    if c_decine == 0 : s += nomeUnita(num)
     elif c_decine == 1 :
-        if num_mod100 == 10 : return 'dieci'
-        elif num_mod100 == 11 : return 'undici'
-        elif num_mod100 == 12 : return 'dodici'
-        elif num_mod100 == 13 : return 'tredici'
-        elif num_mod100 == 14 : return 'quattordici'
-        elif num_mod100 == 15 : return 'quindici'
-        elif num_mod100 == 16 : return 'sedici'
-        elif num_mod100 == 17 : return 'diciassette'
-        elif num_mod100 == 18 : return 'diciotto'
-        elif num_mod100 == 19 : return 'diciannove'
+        if num_mod100 == 10 : s += 'dieci'
+        elif num_mod100 == 11 : s += 'undici'
+        elif num_mod100 == 12 : s += 'dodici'
+        elif num_mod100 == 13 : s += 'tredici'
+        elif num_mod100 == 14 : s += 'quattordici'
+        elif num_mod100 == 15 : s += 'quindici'
+        elif num_mod100 == 16 : s += 'sedici'
+        elif num_mod100 == 17 : s += 'diciassette'
+        elif num_mod100 == 18 : s += 'diciotto'
+        elif num_mod100 == 19 : s += 'diciannove'
     elif c_decine == 2 : # venti
         s += 'vent'
         if c_unita != 1 and c_unita != 8 : s += 'i'
@@ -73,13 +74,22 @@ def nomeDecineUnita(num) :
         s += nomeUnita(num)
     return s
 
-def nomeCentinaia(num) :
+def nomeCentinaiaDecineUnita(num) :
     c_cent = (num // 100) % 10
     s = ''
+    if c_cent > 1 :
+        s += nomeUnita(c_cent)
     if c_cent > 0 :
         s += 'cento'
-        if c_cent > 1 :
-            s += nomeUnita(c_cent)
+    s += nomeDecineUnita(num)
+    print("DEBUG:", s)
+    return s
+
+def nomeCentDecUnitMigliaia(num) :
+    s = nomeCentinaiaDecineUnita(num // 1000)
+    if num // 1000 > 1 : s += 'mila'
+    if num // 1000 == 1 : s = s.replace('uno', 'mille')
+    print("DEBUG:", s)
     return s
 
 
@@ -88,4 +98,6 @@ while True :
     num = inputInteger("Scrivi un numero intero positivo, minore di un milione: ")
     if 0 < num < 1000000 : break
 
-
+# Visualizza
+outputStr = nomeCentDecUnitMigliaia(num) + nomeCentinaiaDecineUnita(num)
+print(outputStr)
